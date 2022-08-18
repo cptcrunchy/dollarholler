0a8ea94c117eb9464f83e35edaabbaa1f095360a
Jason Gutierrez <jason.gutierrez@torchystactos.com>
2022-08-18 16:49:24 -0500

    Add default pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold flex flex-row text-red-800 underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev" target="_blank">kit.svelte.dev</a> to read the documentation</p>
